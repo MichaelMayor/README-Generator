ca3ed85f67bf59294d0dadbb0f96e31d7b0f8ef6
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-10-07 12:24:19 -0400

    Added develop resources

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ inquirer
       // Something else when wrong
     }
   });
+  
+  // array of questions for user
+  const questions = [
+  
+  ];
+  
+  // function to write README file
+  function writeToFile(fileName, data) {
+  }
+  
+  // function to initialize program
+  function init() {
+  
+  }
+  
+  // function call to initialize program
+  init();
+  
