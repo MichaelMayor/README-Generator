101ba5fce7a3b297d50301bb5bf6338ccec6b9c3
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-10-07 21:44:25 -0400

    Added main README sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,39 +6,102 @@ function generateMarkdown(userResponses, userInfo) {
   let draftToC = `## Table of Contents
 
   * [Description](#description)`;
-  
-  if (userResponses.installation !== '') { draftToC += `
-  * [Installation](#installation)` };
 
-  if (userResponses.usage !== '') { draftToC += `
-  * [Usage](#usage)` };
+  if (userResponses.installation !== '') {
+    draftToC += `
+  * [Installation](#installation)`
+  };
 
-  if (userResponses.contributing !== '') { draftToC += `
-  * [Contributing](#contributing)` };
+  if (userResponses.usage !== '') {
+    draftToC += `
+  * [Usage](#usage)`
+  };
 
-  if (userResponses.tests !== '') { draftToC += `
-  * [Tests](#tests)` };
+  if (userResponses.contributing !== '') {
+    draftToC += `
+  * [Contributing](#contributing)`
+  };
+
+  if (userResponses.tests !== '') {
+    draftToC += `
+  * [Tests](#tests)`
+  };
 
   draftToC += `
   * [License](#license)`
 
- // Generate markdown the README Title, License Badge, and Repo badge
- let draftReadme = 
- `# ${userResponses.title}
+  // Generate markdown the README Title, License Badge, and Repo badge
+  let draftReadme =
+    `# ${userResponses.title}
 
  ![GitHub Last Commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo})![License: ${userResponses.license}](https://img.shields.io/badge/License-${userResponses.license}-blue.svg)`
 
- // Add Table of Contents to markdown
- draftReadme += draftToC;    
+  // Add Table of Contents to markdown
+  draftReadme += draftToC;
 
- draftReadme +=
- `
+  draftReadme +=
+    `
  
  ## Description 
  
  
  ${userResponses.description}`
 
+  // Installation section
+  if (userResponses.installation !== '') {
+
+    draftReadme +=
+      `
+      
+      ## Installation
+      
+      ${userResponses.installation}`
+  };
+
+
+  // Usage section
+  if (userResponses.usage !== '') {
+
+    draftReadme +=
+
+      `
+      
+      ## Usage 
+      
+      ${userResponses.usage}`
+  };
+
+
+  // Contributing section
+  if (userResponses.contributing !== '') {
+    `
+      
+      ## Contributing
+      
+      ${userResponses.contributing}`
+  };
+
+
+  // Tests section
+  if (userResponses.tests !== '') {
+
+    draftReadme +=
+      `
+      
+      ## Tests
+      
+      ${userResponses.tests}`
+  };
+
+
+  // License section
+  draftReadme +=
+    `
+      
+      ## License
+      
+      ${userResponses.license}
+      `;
 }
 
 module.exports = generateMarkdown;
